7816851e5bf78c4342a6de667edecf40dc116890
alekseyleshko <alekseyleshko@gmail.com>
2014-04-13 21:42:48 +0400

    Implemented parse slide

--- js/testimonial.js
+++ js/testimonial.js
@@ -29,8 +29,23 @@ Testimonial.prototype = {
 
   parseSlides: function() {
     var $slides = this.$container.children();
-
     this.removeInitialSlides($slides);
+
+    for (var i = 0; i < $slides.length; i++) {
+      var $slide = $($slides[i]);
+
+      var data = this.parseAuthorDiv($slide.children('.author'));
+      data.quote = $slide.children('.quote').text().trim();
+      this.slides.push(data);
+    }
+  },
+
+  parseAuthorDiv: function($container) {
+    var authorInfo = { fullName: $container.children('.full_name').text().trim(),
+      company: $container.children('.company').text().trim(),
+      fotoSrc: $container.children('.foto').attr('src')
+    };
+    return authorInfo;
   },
 
   removeInitialSlides: function($slides) {
