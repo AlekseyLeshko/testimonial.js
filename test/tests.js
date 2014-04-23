afcb5c44bcbb3574ebe8214cf27bf55ae0237c79
alekseyleshko <alekseyleshko@gmail.com>
2014-04-23 12:24:26 +0400

    Update

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,25 @@
+test('Testimonial constructor', function() {
+  var $container = $('#qunit-fixture .testimonial_slider').first();
+  var timeout = 7000;
+  var autostart = true;
+
+  var testimonial = new Testimonial($container);
+  ok(testimonial.$container === $container, 'Plugin container ok');
+  ok(testimonial.pluginOptions.timeout == timeout, 'Plugin default timeout option ok');
+  ok(testimonial.pluginOptions.autostart == autostart, 'Plugin default autostart option ok');
+
+  ok(testimonial.timerId !== undefined, 'Autostart works');
+  testimonial.stop();
+
+  ok(testimonial.$slidesWrapper.attr('class') === 'main_container', 'Slides wrapper create');
+
+  ok(testimonial.$slides.length == 3, 'Count slides ok');
+  ok(testimonial.currentSlideIndex == 0, 'Current index ok');
+});
+
 test('Test example', function() {
   var $container = $('#qunit-fixture .testimonial_slider').first();
-  console.log($container);
+  console.log($container.html());
 
   equal(1, 1, '1 == 1')
 });
