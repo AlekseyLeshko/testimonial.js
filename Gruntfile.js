fb642b4dc201e5da8a65644e8a4957cbc0ab2b9e
alekseyleshko <alekseyleshko@gmail.com>
2014-05-01 21:14:45 +0400

    Use grunt-contrib-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,20 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     qunit: {
       all: ['test/index.html']
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/js/*.js'],
+        dest: 'dist/<%= pkg.name.toLowerCase() %>'
+      }
     }
   });
 
