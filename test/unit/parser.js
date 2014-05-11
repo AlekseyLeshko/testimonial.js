7295fc179fe94322742559f46eb0db3bdc118e31
alekseyleshko <alekseyleshko@gmail.com>
2014-05-11 11:30:54 +0400

    Add test for plugin parser

--- test/unit/parser.js
+++ test/unit/parser.js
@@ -1,4 +1,9 @@
 module('Tests for the parser plugin');
-test('Test#1', function() {
-  ok(true, 'True');
+test('Get attr href', function() {
+  var url = 'https://github.com/AlekseyLeshko/testimonial.js';
+  var $node = $('<a />', {
+    href: url
+  });
+  var href = Parser.prototype.getAttrHrefOrDefault($node);
+  ok(href === url, 'Received href is correct');
 });
