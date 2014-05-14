8599f4f795f0b2ff35ee5660c4269142f28419ab
alekseyleshko <alekseyleshko@gmail.com>
2014-05-14 11:28:14 +0400

    Refactoring test

--- test/unit/testimonial.js
+++ test/unit/testimonial.js
@@ -1,27 +1,20 @@
 module('Testimonial tests');
-test('Testimonial constructor', function() {
-  var $container = $('#qunit-fixture .testimonial_slider').first();
-  var timeout = 7000;
-  var autostart = true;
-
-  var testimonial = new Testimonial($container);
-  ok(testimonial.$container === $container, 'Plugin container ok');
-  ok(testimonial.pluginOptions.timeout == timeout, 'Plugin default timeout option ok');
-  ok(testimonial.pluginOptions.autostart == autostart, 'Plugin default autostart option ok');
-
-  ok(testimonial.timerId !== undefined, 'Autostart works');
-  testimonial.stop();
-
-  ok(testimonial.$slidesWrapper.attr('class') === 'main_container', 'Slides wrapper create');
-
-  ok(testimonial.$slides.length == 3, 'Count slides ok');
-  ok(testimonial.currentSlideIndex == 0, 'Current index ok');
-});
-
 var defaultOptions = {
   timeout: 7000,
   autostart: true
 };
+
+test('Testimonial constructor', function() {
+  var $container = $('#qunit-fixture .testimonial_slider').first();
+  var testimonial = new Testimonial($container);
+
+  ok(testimonial.$container === $container, 'Plugin container ok');
+  ok(testimonial.pluginOptions.timeout == defaultOptions.timeout, 'Plugin default timeout option ok');
+  ok(testimonial.pluginOptions.autostart == defaultOptions.autostart, 'Plugin default autostart option ok');
+  ok(testimonial.$slides.length == 3, 'Count slides ok');
+  ok(testimonial.dataArr.length == 3);
+  ok(testimonial.currentSlideIndex == 0, 'Current index ok');
+});
 
 test('Get default options', function() {
   var pluginOptions = Testimonial.prototype.getDefaultOptions();
