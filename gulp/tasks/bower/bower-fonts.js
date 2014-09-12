34ddf532605b059a5752ede361cab66b883d59be
alekseyleshko <alekseyleshko@gmail.com>
2014-09-12 12:51:55 +0400

    Remove bower

--- gulp/tasks/bower/bower-fonts.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('bower-fonts', function(cb) {
-  var arr = [];
-
-  return gulp.src(arr)
-    .pipe(gulp.dest('www/fonts'));
-});
--- gulp/tasks/main/run.js
+++ gulp/tasks/main/run.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('run', function(callback) {
-  return runSequence(['bower', 'build', 'connect', 'watch'], 'open-index', callback);
+  return runSequence(['build', 'connect', 'watch', 'open-index', 'tdd'], callback);
 });
