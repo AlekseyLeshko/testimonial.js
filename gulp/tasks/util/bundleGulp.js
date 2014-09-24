5c559444ef40fecab21444248406afbbc782f8b8
alekseyleshko <alekseyleshko@gmail.com>
2014-09-24 18:57:26 +0400

    Add getBanner function

--- gulp/tasks/util/bundleGulp.js
+++ gulp/tasks/util/bundleGulp.js
@@ -2,4 +2,15 @@ var util = require('gulp-util');
 var appConfig = require('../../../config/app.json');
 
 module.exports = {
+  getBanner: function() {
+    var banner = ['/**',
+      '  * <%= pkg.name %> - <%= pkg.description %>',
+      '  * @version v<%= pkg.version %>',
+      '  * @link <%= pkg.homepage %>',
+      '  * @license <%= pkg.license %>',
+      '*/',
+      ''
+    ].join('\n');
+    return banner;
+  }
 };
