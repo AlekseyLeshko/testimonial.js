917ac3b904a50cb0c70eebb0c22fdd7a25940c74
alekseyleshko <alekseyleshko@gmail.com>
2014-09-24 17:56:42 +0400

    Update scripts task

--- gulp/tasks/js/scripts.js
+++ gulp/tasks/js/scripts.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var rename = require("gulp-rename");
 
 gulp.task('scripts', ['jshint'], function() {
   var config = {
@@ -10,8 +11,12 @@ gulp.task('scripts', ['jshint'], function() {
 
   return gulp.src('src/js/**/*.js')
     .pipe(sourcemaps.init())
+    .pipe(concat('testimonial.js'))
+    .pipe(gulp.dest('dist/js/'))
     .pipe(uglify(config))
-    .pipe(concat('testimonial.min.js'))
+    .pipe(rename('testimonial.min.js'))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rename('testimonial.min.map.js'))
     .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest('dist/js/'));
 });
