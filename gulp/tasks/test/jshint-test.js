1ba3422c0a00ea86ee557cd9f8c7a14c3c36bcf9
alekseyleshko <alekseyleshko@gmail.com>
2014-12-18 14:16:18 +0300

    Add jshint-test gulp task

---
+++ gulp/tasks/test/jshint-test.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+gulp.task('jshint-test', function() {
+  var paths = [
+    'test/unit/**/*.js'
+  ];
+
+  var jshintConfig = {
+    globals: {
+      describe: false,
+      beforeEach: false,
+      it: false,
+      expect: false,
+      spyOn: false,
+      $: false,
+      TestimonialSlide: false,
+      jasmine: false,
+      loadFixtures: false,
+      Parser: false
+    }
+  };
+  return gulp.src(paths)
+    .pipe(jshint(jshintConfig))
+    .pipe(jshint.reporter(stylish));
+});
