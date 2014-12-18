34b885880a6756bfc8e6f8474b6e39573e921d6f
alekseyleshko <alekseyleshko@gmail.com>
2014-12-18 16:57:11 +0300

    Add tests

--- test/unit/testimonial.spec.js
+++ test/unit/testimonial.spec.js
@@ -1,10 +1,43 @@
 'use strict';
 
 describe('Testimonial', function() {
+
   beforeEach(function() {
   });
 
-  it('should', function() {
-    expect(0).toEqual(0);
+  it('should indexing return 2', function() {
+    Testimonial.prototype.$slides = [1, 2 , 3];
+    Testimonial.prototype.currentSlideIndex = 1;
+
+    Testimonial.prototype.indexing();
+
+    var res = Testimonial.prototype.currentSlideIndex;
+    expect(res).toEqual(2);
+  });
+
+  it('should indexing return 0', function() {
+    Testimonial.prototype.$slides = [];
+
+    Testimonial.prototype.indexing();
+
+    var res = Testimonial.prototype.currentSlideIndex;
+    expect(res).toEqual(0);
+  });
+
+  it('should indexing zeroed currentSlideIndex', function() {
+    Testimonial.prototype.$slides = [1, 2 , 3];
+    Testimonial.prototype.currentSlideIndex = 2;
+
+    Testimonial.prototype.indexing();
+
+    var res = Testimonial.prototype.currentSlideIndex;
+    expect(res).toEqual(0);
+  });
+
+  it('should getDefaultOptions', function() {
+    var options = Testimonial.prototype.getDefaultOptions();
+
+    expect(options.timeout).toEqual(7000);
+    expect(options.autostart).toBeTruthy();
   });
 });
