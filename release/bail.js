f9ed10d7fb5943e82de2f7f712258ef6ba248c99
alekseyleshko <alekseyleshko@gmail.com>
2015-01-16 12:09:12 +0300

    Remove release folder

--- release/bail.js
+++
@@ -1,8 +0,0 @@
-function bail(msg) {
-  var prexif = '[check-config-files] ';
-  process.stderr.write(prexif + msg + '\n');
-  console.log(false);
-  process.exit(1);
-}
-
-module.exports = bail;
--- release/get-version.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-var semver = require("semver");
-
-var packageFile = fs.readFileSync("package.json");
-var packageJson = JSON.parse(packageFile);
-var ver = semver.parse(packageJson.version);
-console.log(ver.toString());
