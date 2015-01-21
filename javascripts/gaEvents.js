664cc1bc5679703d40ec15a4270c7ac458734d2c
alekseyleshko <alekseyleshko@gmail.com>
2015-01-21 12:23:04 +0300

    Add ga target for next button

--- javascripts/gaEvents.js
+++ javascripts/gaEvents.js
@@ -1,9 +1,20 @@
 function createGAEvents() {
   githubRibbon();
+  nextButtons();
 }
 
 function githubRibbon() {
   $('#githubRibbon').click(function() {
     ga('send', 'event', 'Crossing', 'ClickOnForkMe');
+  });
+}
+
+function nextButtons() {
+  var actionFirstPart = 'ClickOnNextButton_';
+
+  $(document).on('click', '.next_slide', function() {
+    var name = $(this).parent().attr('id');
+    var action = actionFirstPart + name;
+    ga('send', 'event', 'Clicking', action);
   });
 }
